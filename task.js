506454ca286b4ad350ffe42d875f8ebbc67a1e72
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-28 17:52:31 +0100

    updated

--- task.js
+++ task.js
@@ -9,11 +9,6 @@
 // E.g [‘a’,’b’,’c’,’d’,’f’] =>’e’
 // [‘O’,’Q’,’R’,’S’]=>’P’
 
-// function missingLetters(arr){
-//     let alphabets = "^[a-z]+S"
-//     if(arr )
-// } 
-
 
 function missingLetters(arr){
    let letters =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
